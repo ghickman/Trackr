7c70584578bc43457fc56837b3e8389d9637d551
George Hickman <ghickman@madnashua.co.uk>
2009-03-13 12:08:06 +0000

    added twitter config file

---
+++ config/twitter.php
@@ -0,0 +1,12 @@
+<?php
+$config['Twitter'] = array(
+    'madnashua'=>array(
+        'username' => 'madnashua',
+        'password' => 'P8gU-a8a_!'
+    ),
+    'tellannDev'=>array(
+        'username' => 'tellannDev',
+        'password' => 'Jil)(23af9'
+    )
+);
+?>
