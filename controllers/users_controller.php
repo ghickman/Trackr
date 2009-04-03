b802e421e4fdc40cb4a41e0692317ebc5db3e27e
George Hickman <ghickman@madnashua.co.uk>
2009-04-03 16:55:00 +0100

    destroy session on logout

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -26,6 +26,7 @@ class UsersController extends AppController {
 	 */
 	function logout() {
 		$this->Session->setFlash('Good-Bye');
+		$this->Session->destroy();
 		$this->redirect($this->Auth->logout());
 	}
 
