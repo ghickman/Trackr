b829ba5934750a30c074064252cb1acb3a8e7fe2
George Hickman <ghickman@madnashua.co.uk>
2009-04-07 08:48:00 +0100

    removed condition from database config file and added extra databases. added app_model to try and set db depending on the environment. added setup.sh for setting up trackr.ghickman.co.uk

--- config/database.php
+++ config/database.php
@@ -1,24 +1,31 @@
 <?php
 class DATABASE_CONFIG {
-    if(env('HTTP_HOST') == Configure::read('env.dev')) {
-    	var $default = array(
-    		'driver' => 'mysql',
-    		'persistent' => false,
-    		'host' => 'localhost',
-    		'login' => 'root',
-    		'password' => 'Jil)(23af9',
-    		'database' => 'tellannDev'
-    	);
-	} elseif(env('HTTP_HOST') == Configure::read('env.prod')) {
-	    var $default = array(
-    		'driver' => 'mysql',
-    		'persistent' => false,
-    		'host' => 'localhost',
-    		'login' => 'root',
-    		'password' => 'Jil)(23af9',
-    		'database' => 'tellann'
-    	);
-	}
+	var $default = array(
+    	'driver' => 'mysql',
+    	'persistent' => false,
+    	'host' => 'localhost',
+    	'login' => 'root',
+    	'password' => 'Jil)(23af9',
+    	'database' => 'tellannDev'
+    );
+	
+	var $development = array(
+    	'driver' => 'mysql',
+    	'persistent' => false,
+    	'host' => 'localhost',
+    	'login' => 'root',
+    	'password' => 'Jil)(23af9',
+    	'database' => 'tellannDev'
+    );
+    
+    var $production = array(
+    	'driver' => 'mysql',
+    	'persistent' => false,
+    	'host' => 'localhost',
+    	'login' => 'root',
+    	'password' => 'Jil)(23af9',
+    	'database' => 'tellann'
+    );
 	
     var $test = array(
     	'driver' => 'mysql',
