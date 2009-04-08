efb95455347c58f7627478517a294f38019b2b01
George Hickman <ghickman@madnashua.co.uk>
2009-04-08 10:10:07 +0100

    removed unneeded entries from database.php

--- config/database.php
+++ config/database.php
@@ -9,24 +9,6 @@ class DATABASE_CONFIG {
     	'database' => 'tellannDev'
     );
 	
-	var $development = array(
-    	'driver' => 'mysql',
-    	'persistent' => false,
-    	'host' => 'localhost',
-    	'login' => 'root',
-    	'password' => 'Jil)(23af9',
-    	'database' => 'tellannDev'
-    );
-    
-    var $production = array(
-    	'driver' => 'mysql',
-    	'persistent' => false,
-    	'host' => 'localhost',
-    	'login' => 'root',
-    	'password' => 'Jil)(23af9',
-    	'database' => 'tellann'
-    );
-	
     var $test = array(
     	'driver' => 'mysql',
     	'persistent' => false,
